46ff21aa63601372fc8367f39ced7429b58dd7c9
Matthew Marcus <osirun@gmail.com>
2017-03-16 18:23:41 +0000

    toArabic logic

--- src/Acme/Arabicizer.php
+++ src/Acme/Arabicizer.php
@@ -6,10 +6,34 @@ class Arabicizer
 {
     public function toArabic($n)
     {
-        if ($n === "") return 0;
+        $trans = array_flip([
+          1000 => 'M',
+           900 => 'CM',
+           500 => 'D',
+           400 => 'CD',
+           100 => 'C',
+            90 => 'XC',
+            50 => 'L',
+            40 => 'XL',
+            10 => 'X',
+             9 => 'IX',
+             5 => 'V',
+             4 => 'IV',
+             1 => 'I'
+        ]);
+
+        if (strlen($n) === 0) return 0;
+        if (strlen($n) === 1) return $trans[$n];
+
+        $firstTwoChars = substr($n, 0, 2);
+        if (array_key_exists($firstTwoChars, $trans)) {
+          $tail = substr($n, 2);
+          return $trans[$firstTwoChars] + $this->toArabic($tail);
+        }
+
         $head = $n[0];
         $tail = substr($n, 1);
 
-        if ($head == "I") return 1 + $this->toArabic($tail);
+        return $trans[$head] + $this->toArabic($tail);
     }
 }
